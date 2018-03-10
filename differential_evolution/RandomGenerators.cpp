7483a6945eb971338d4eaf6eefcde08f99a1f584
Peter <peterhj@gmail.com>
2018-03-10 12:09:38 +0100

    unsigned long

---
+++ differential_evolution/RandomGenerators.cpp
@@ -0,0 +1,9 @@
+//
+//  RandomGenerators.cpp
+//  differential_evolution
+//
+//  Created by Peter Helstrup Jensen on 10/03/2018.
+//  Copyright © 2018 Peter Helstrup Jensen. All rights reserved.
+//
+
+#include "RandomGenerators.hpp"
