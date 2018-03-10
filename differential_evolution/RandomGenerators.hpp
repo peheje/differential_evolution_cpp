dd49a3d82a65a9b33fb11fef5788a83536f7aba7
Peter <peterhj@gmail.com>
2018-03-10 12:33:20 +0100

    Revert "Revert "move random into files""
    This reverts commit 546ebafe97631f330a860d7d71891f0e96390b6a.

---
+++ differential_evolution/RandomGenerators.hpp
@@ -0,0 +1,15 @@
+//
+//  RandomGenerators.hpp
+//  differential_evolution
+//
+//  Created by Peter Helstrup Jensen on 10/03/2018.
+//  Copyright © 2018 Peter Helstrup Jensen. All rights reserved.
+//
+
+#ifndef RandomGenerators_hpp
+#define RandomGenerators_hpp
+
+double fRand(double min, double max);
+unsigned long iRand(unsigned long min, unsigned long max);
+
+#endif /* RandomGenerators_hpp */
